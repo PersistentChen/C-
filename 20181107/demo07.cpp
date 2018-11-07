4e86623f66adcc421c72e25bea6c691fd02373da
Persistent <178766869@qq.com>
2018-11-08 07:53:55 +0800

    20181107

---
+++ 20181107/demo07.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+	char greeting[6] = {'H', 'E', 'L', 'L', 'O', '\O'};
+	
+	cout << "Greeting message: ";
+	cout << greeting << "." << endl;
+	
+	return 0;
+}
